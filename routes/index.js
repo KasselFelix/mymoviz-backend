e348cd8b54e14133e74b1429001bfc68fb12926f
KasselFelix <w-kassel.f@outlook.fr>
2024-07-10 14:57:11 +0200

    first commit

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+const API_KEY= process.env.API_KEY;
+
+router.get('/movies',(req,res) =>{
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`)
+    .then(response => response.json())
+    .then(movies => res.json({movies:movies.results}))
+})
+
+module.exports = router;
---
+++ urls.js
@@ -0,0 +1,7 @@
+// Insert the backend URL given by Vercel in this variable
+const BACKEND_URL = 'https://mymoviz-backend-9xmt1kstg-kasselfelixs-projects.vercel.app/';
+
+// Insert the frontend URL given by Vercel in this variable
+const FRONTEND_URL = 'https://mymoviz-frontend-cyan-nine.vercel.app/';
+
+module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
